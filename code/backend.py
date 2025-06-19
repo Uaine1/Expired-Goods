df9ffba63a1b77fd7a0b00d46d238c816d3aafbb
Uaine1 <angeloalcantara243@gmail.com>
2025-06-19 11:16:05 +0800

    Add a database

--- code/backend.py
+++ code/backend.py
@@ -1,14 +1,11 @@
 from datetime import date, datetime
+import sqlite3
 
+# Should be running in the background
 def check_date(item_name, expiry_date):
-    #userinput = userinput.strip()
-
-    try:
-        expiry_date = datetime.strptime(expiry_date, "%d/%m/%y").date() #converts date strings to actual dates
-    except ValueError:
-        return "Invalid date format, use dd/mm/yy. "
-    
     current_date = date.today()
+    
+    store_items(item_name, current_date, expiry_date)
     
     if current_date == expiry_date:
         return(f"{item_name} Food has expired today: {current_date}")
@@ -18,3 +15,53 @@ def check_date(item_name, expiry_date):
     
     else:
         return(f"{item_name} is safe until {expiry_date}")
+    
+    
+def store_items(item_name, expiry_date):
+    try:
+        expiry_date = datetime.strptime(expiry_date, "%d/%m/%y").date() #converts date strings to actual dates
+    except ValueError:
+        return "Invalid date format, use dd/mm/yy. "
+    
+    date_stored = date.today()
+    
+    connection = sqlite3.connect("items.db")
+    cursor = connection.cursor()
+    
+    #storing_item = 
+    
+    cursor.execute("""CREATE TABLE IF NOT EXISTS items(
+        item_id INTEGER PRIMARY KEY,
+        item_name TEXT,
+        date_stored DATE,
+        expiry_date DATE)""")
+    
+    cursor.execute("INSERT INTO items (item_name, date_stored, expiry_date) VALUES(?,?,?)", (item_name, date_stored, expiry_date))
+    
+    connection.commit()
+    
+    """cursor.execute("SELECT * FROM items")
+    item_rows = cursor.fetchall()
+    
+    for row in item_rows:
+        return(row)"""
+        
+
+def show_items():
+    connection = sqlite3.connect("items.db")
+    cursor = connection.cursor()
+    
+    cursor.execute("SELECT item_name, expiry_date FROM items")
+    item_row = cursor.fetchall()
+    
+    connection.close()
+    
+    if not item_row:
+        return "No items found"
+    
+    items_list = "Stored item:\n"
+    
+    for item_name, expiry_date in item_row:
+        items_list += f"{item_name}: Expires on {expiry_date}\n"
+        
+    return items_list
--- code/main.py
+++ code/main.py
@@ -1,9 +1,23 @@
-from backend import check_date
+from backend import store_items, show_items
 
 def main():
-    user_item = input("Enter Item Name: ")
-    item_date = input("Enter Items Expiration Date:")
-    print(check_date(user_item,item_date))
+    while True:
+        print("A - add item, C - check item expiry, X - exit")
+        user_options = input("Enter a command: ").strip()
+        
+        match user_options:
+            case "A":
+                user_item = input("Enter Item Name: ")
+                item_date = input("Enter Items Expiration Date:")
+                store_items(user_item,item_date)
+                print("Item Stored")
+            
+            case "C":
+                print(show_items())
+                
+            case "X":
+                print("Closing Software")
+                break
         
 
 if __name__ == "__main__":
